6da7300f3f1086a1801fe37da7bcd139be01c3e3
sctech <106792792+sctech5@users.noreply.github.com>
2022-07-12 10:40:38 +0700

    Update script.js

--- res/script.js
+++ res/script.js
@@ -1,5 +1,3 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoibWFsLXdvb2QiLCJhIjoiY2oyZ2t2em50MDAyMzJ3cnltMDFhb2NzdiJ9.X-D4Wvo5E5QxeP7K_I3O8w';
-
 const el=a=>{
 	const b=document.createElement(a.a);
 	a.b && a.b.appendChild(b);
@@ -65,29 +63,32 @@ const divTop=a=>{
 	el({a:'span', b:topRight.children[2]}).textContent = 'PERFORMANCE';
 	
 	const switchArea=a=>{
+		map1.loader({ a:`${map1.dir1}marker/marker${document.querySelector('#NavLeft').children[1].getAttribute('data-a')}-${a.target.value}.json`, b:map1.markerLoader });
+		/*
 		map1.area = a.target.value;
 		//map.getSource(map1.src1).setData(map1.dt);
 		mapUnloadData(map1.src1);
 		addSrc1({a:map1.src1, b:map1.dt});
+		*/
 	}
 	
 	el({a:'table', b:topRight.children[0]});
 	el({a:'tr', b:topRight.children[0].children[1]});
 	el({a:'td', b:topRight.children[0].children[1].children[0]}).textContent = 'Area';
 	var label = el({a:'label', b:el({a:'td', b:topRight.children[0].children[1].children[0]}), c:{class:'switch'}});
-	el({a:'input', b:label, c:{type:'radio', name:'area1', value:'a1', checked:true}}).onchange = switchArea;
+	el({a:'input', b:label, c:{type:'radio', name:'area1', value:'1', checked:true}}).onchange = switchArea;
 	el({a:'span', b:label, c:{class:'slider'}});
 	
 	el({a:'tr', b:topRight.children[0].children[1]});
 	el({a:'td', b:topRight.children[0].children[1].children[1]}).textContent = 'Telkom Regional';
 	var label = el({a:'label', b:el({a:'td', b:topRight.children[0].children[1].children[1]}), c:{class:'switch'}});
-	el({a:'input', b:label, c:{type:'radio', name:'area1', value:'a2'}}).onchange = switchArea;
+	el({a:'input', b:label, c:{type:'radio', name:'area1', value:'2'}}).onchange = switchArea;
 	el({a:'span', b:label, c:{class:'slider'}});
 	
 	el({a:'tr', b:topRight.children[0].children[1]});
 	el({a:'td', b:topRight.children[0].children[1].children[2]}).textContent = 'TSel Regional';
 	var label = el({a:'label', b:el({a:'td', b:topRight.children[0].children[1].children[2]}), c:{class:'switch'}});
-	el({a:'input', b:label, c:{type:'radio', name:'area1', value:'a3'}}).onchange = switchArea;
+	el({a:'input', b:label, c:{type:'radio', name:'area1', value:'3'}}).onchange = switchArea;
 	el({a:'span', b:label, c:{class:'slider'}});
 	
 	el({a:'table', b:topRight.children[2]});
@@ -117,12 +118,20 @@ const divTop=a=>{
 	//================  end topright  ==================================
 }
 
+const NavLeftSwitch=a=>{
+	const b = document.querySelector('#NavLeft > div > img.checked');
+	b && b.classList.remove('checked');
+	a.target.classList.add('checked');
+	a.target.parentElement.setAttribute('data-a', a.target.getAttribute('data-a'));
+	map1.loader({ a:`${map1.dir1}marker/marker${a.target.getAttribute('data-a')}-${document.querySelector('input[name="area1"]:checked').value}.json`, b:map1.markerLoader });
+}
+
 const NavLeft=a=>{
 	const b=el({a:'div', b:a, c:{id:'NavLeft'}});
 	el({a:'img', b:el({a:'div', b:b}), c:{src:'res/icons/Search2.svg'}});
 	
-	el({a:'img', b:el({a:'div', b:b}), c:{src:'res/icons/cx.svg', class:'btn1 checked'}}).onclick = NavLeftSwitch;
-	el({a:'img', b:b.children[1], c:{src:'res/icons/np.svg', class:'btn1'}}).onclick = NavLeftSwitch;
+	el({a:'img', b:el({a:'div', b:b, c:{'data-a':''}}), c:{src:'res/icons/cx.svg', class:'btn1 checked', 'data-a':''}}).onclick = NavLeftSwitch;
+	el({a:'img', b:b.children[1], c:{src:'res/icons/np.svg', class:'btn1', 'data-a':-1}}).onclick = NavLeftSwitch;
 	el({a:'img', b:b.children[1], c:{src:'res/icons/gp.svg', class:'btn1'}}).onclick = NavLeftSwitch;
 	el({a:'img', b:b.children[1], c:{src:'res/icons/Report2.svg', class:'btn1'}}).onclick = NavLeftSwitch;
 	
@@ -218,11 +227,69 @@ const hiChartJs=a=>{
 	el({a:'span', b:b}).textContent = 'Last Update: 20.05';
 }
 
+const addMarker1=a=>{
+	const b = el({a:'div', c:{style:'width:150px;height:200px;'}});
+	const c = Highcharts.chart(b, {
+		chart: {
+			backgroundColor: 'rgba(0,0,0,0)',
+			type: 'column'
+		},
+		title: {
+			text: a.title,//'AREA I',
+			verticalAlign: 'bottom',
+			y: 6,
+			style: { 'font-weight': 'bold', fontSize: '24px'}
+		},
+		legend: {
+			enabled: false
+		},
+		xAxis: [{ visible: false }],
+		yAxis: [{ visible: false }],
+		series: [{
+			name: 'Population',
+			data: a.data,
+			/*
+			[
+				['TEL', 1.7],
+				['SMRT', 1.2],
+				['IND', 0.9]
+			],*/
+		dataLabels: [{
+				enabled: true,
+				format: '{key}',
+				align: 'center'
+			}, {
+				enabled: true,
+				color: '#FFFFFF',
+				inside: true,
+				verticalAlign: 'top',
+				align: 'center',
+				//style: {
+				//    fontSize: '13px',
+				//    fontFamily: 'Verdana, sans-serif'
+				//}
+			}],
+			zoneAxis: 'x',
+			zones: [{value:1,color: '#FD2D6C'},{color: '#515151'}],
+			groupPadding: 0,
+			shadow: true
+		}]
+	});
+	b.children[0].style.transform = 'scale(0.8,1)';
+	console.log(b.children[0]);
+	new mapboxgl.Marker({
+		element: b,
+		draggable:true})
+	.setLngLat([120, 1])
+	.addTo(map);
+}
+
 const map1 = {
-	src1: '../data/province.json',//'area.json',
-	lvl: 1,
-	next: 'ADM2_PCODE',
-	dt: null,
+	dir1: 'data/',
+	srcMap: { type:"FeatureCollection", features:[] },
+	srcLines: { type:"FeatureCollection", features:[{type: 'Feature', properties: {}, geometry: { type: 'MultiLineString', coordinates: [[]] }}] },
+	srcText: { type:"FeatureCollection", features:[] },
+	marker: [],
 	area: 'a1',
 	Prop: {
 		Name: 'ADM1_PCODE',//'nama',
@@ -233,86 +300,157 @@ const map1 = {
 			a3: [0, 2, 4, 1, 0, 2, 3, 4, 0, 1, 3, 2, 1]
 		}
 	},
-	selected: 0,
-	hoveredStateId: null,
+	mapHover: null,
+	mapSelect: '',
+	markerCX: a=>{
+		const b = el({a:'div', c:{class:'markerCX', 'data-a':`${map1.marker.length} `}});
+		el({a:'h4', b:b}).textContent = a.title;
+		el({a:'p', b:b, c:{class:'markerCXbold'}}).textContent = '#1 Telkomsel';
+		el({a:'p', b:b}).textContent = '#2 Indosat';
+		el({a:'p', b:b}).textContent = '#3 XL';
+		const c = new mapboxgl.Marker({ element: b }).setLngLat(a.co).addTo(map);
+		map1.marker.push([c]);
+	},
+	markerNP: a=>{
+		const b = el({a:'div', c:{style:'width:150px;height:200px;', 'data-a':`${map1.marker.length} `}});
+		const c = Highcharts.chart(b, {
+			chart: {
+				backgroundColor: 'rgba(0,0,0,0)',
+				type: 'pie'
+			},
+			title: { text: '' },
+			legend: { enabled: false },
+			series: [{
+				innerSize: '50%',
+				data: [100-a.v, a.v],
+				dataLabels: [{ enabled: false }]
+			}]
+		});
+		b.children[0].style.transform = 'scale(0.6,0.6)';
+		const d = new mapboxgl.Marker({ element: b }).setLngLat(a.co).addTo(map);
+		map1.marker.push([d, c]);
+	},
 	mapOnMouseenter: a=>{
-		if (map1.src1 !== null && a.features.length > 0) {
-			if (map1.hoveredStateId) {
+		if (a.features.length > 0) {
+			if (map1.mapHover) {
 				map.setFeatureState(
-					{ source: map1.src1, id: map1.hoveredStateId },
+					{ source: 'mapSource', id: map1.mapHover },
 					{ hover: false }
 				);
 			}
-			elm.title.textContent = a.features[0].properties.nama;
-			map1.hoveredStateId = a.features[0].id;
+			//elm.title.textContent = a.features[0].id+' '+a.features[0].properties.nama;
+			map1.mapHover = a.features[0].id;
 			map.setFeatureState(
-				{ source: map1.src1, id: map1.hoveredStateId },
+				{ source: 'mapSource', id: map1.mapHover },
 				{ hover: true }
 			);
 		}
 	},
 	mapOnMmouseleave: ()=>{
-		if (map1.src1 !== null && map1.hoveredStateId !== null) {
+		if (map1.mapHover !== null) {
 			map.setFeatureState(
-				{ source: map1.src1, id: map1.hoveredStateId },
+				{ source: 'mapSource', id: map1.mapHover },
 				{ hover: false }
 			);
-			elm.title.textContent = 'NATIONAL REGION';
+			//elm.title.textContent = 'NATIONAL REGION';
+			map1.mapHover = null;
 		}
-		map1.hoveredStateId = null;
 	},
 	mapOnClick: a=>{
-		if (map1.src1 !== null && a.features.length > 0) {
-			map1.selected = a.features[0].id;
-			mapLoadData({src:`${map1.selected}.json`, lvl:map1.lvl+1, name: 'nama', old:map1.src1});
+		if (a.features.length > 0) {
+			//map1.selected = a.features[0].id;
+			//mapLoadData({src:`${map1.selected}.json`, lvl:map1.lvl+1, name: 'nama', old:map1.src1});
 		}
+	},
+	loader: a=>{
+		const b = new XMLHttpRequest();
+		b.open('GET', a.a);
+		b.onreadystatechange=()=>{
+			if (b.readyState == 4 && b.status == 200) a.b(b.responseText);
+		}
+		b.send();
+	},
+	markerLoader: a=>{
+		const b = JSON.parse(a);
+		map1.srcLines.features[0].geometry.coordinates = b.lines;
+		map.getSource('lineSource').setData(map1.srcLines);
+		map1.marker.forEach(a=>a[0].remove());
+		map1.marker = [];
+		b.marker.forEach((a,b)=>{(a[0] == 0 ? map1.markerCX({title:a[2], co:a[1]}) : map1.markerNP({v:a[2], co:a[1]})) });
+		map1.srcText.features = [];
+		b.text.forEach((a,b)=> map1.srcText.features.push({type: 'Feature', id:b, properties: {text:a[0]}, geometry: { type: 'Point', coordinates: a[1] }}))
+		map.getSource('textSource').setData(map1.srcText);
+	},
+	init: ()=>{
+		map.setPadding({
+			top: 150,
+			left: 80,
+			bottom: 300,
+			right: 10
+		});
+		map.fitBounds([[93.22614790206802,-9.74291381418648],[141.27123973965843,10.496576109134423]]);
+		
+		map1.loader({
+			a: `${map1.dir1}province.json`,
+			b: a=>{
+				console.log('a');
+				map1.srcMap = JSON.parse(a);
+				map.addSource('mapSource', {
+					type: 'geojson',
+					data: map1.srcMap,
+				});
+				map.addLayer({
+					id: 'mapLayer',
+					type: 'fill',
+					source: 'mapSource',
+					paint: {
+						'fill-color': ['to-color', ['at', ['at', ['get', map1.area], ['literal', map1.Prop.color[map1.area]]], ['literal', map1.Prop.dcolor]]],
+						'fill-opacity': [ 'case', ['boolean', ['feature-state', 'hover'], false], 0.9, 1],
+						'fill-outline-color': 'white'
+					}
+				});
+				map.on('mouseenter', 'mapLayer', map1.mapOnMouseenter);
+				map.on('mouseleave', 'mapLayer', map1.mapOnMmouseleave);
+				map.on('click', 'mapLayer', map1.mapOnClick);
+				
+				map.addSource('lineSource', {
+					type: 'geojson',
+					data: map1.srcLines
+				});
+				map.addLayer({
+					id: 'lineLayer',
+					type: 'line',
+					source: 'lineSource',
+					paint: {
+						'line-width': 2
+					}
+				});
+				map.addSource('textSource', {
+					type: 'geojson',
+					data: map1.srcText
+				});
+				map.addLayer({
+					id: 'textLayer',
+					type: 'symbol',
+					source: 'textSource',
+					layout: {
+						'text-field': ['get', 'text'],
+						//'text-variable-anchor': ['top', 'bottom', 'left', 'right'],
+						'text-size': 12,
+						'text-justify': 'auto'
+					}
+				});
+				
+				map1.loader({ a:`${map1.dir1}marker/marker-1.json`, b:map1.markerLoader });
+			}
+		});
 	}
 }
 
-const addSrc1=a=>{
-	map.addSource(a.a, {
-		type: 'geojson',
-		data: a.b//,
-		//generateId: true
-	});
-	map.addLayer({
-		id: `f${a.a}`,
-		type: 'fill',
-		source: a.a,
-		paint: {
-			'fill-color': ['to-color', ['at', ['at', ['get', map1.area], ['literal', map1.Prop.color[map1.area]]], ['literal', map1.Prop.dcolor]]],
-			'fill-opacity': [ 'case', ['boolean', ['feature-state', 'hover'], false], 0.9, 1],
-			'fill-outline-color': 'white'
-		}
-	}, 'country-label');
-	/*
-	map.addLayer({
-		id: `s${a.a}`,
-		type: 'symbol',
-		source: a.a,
-		layout: {
-			'text-field': ['get', 'ADM1_EN'],
-			//'text-variable-anchor': ['top', 'bottom', 'left', 'right'],
-			//'text-radial-offset': 0.5,
-			'text-justify': 'auto'
-		}
-	});
-	*/
-	map.on('mousemove', `f${a.a}`, map1.mapOnMouseenter);
-	//map.on('mouseleave', `f${a.a}`, map1.mapOnMmouseleave);
-	map.on('click', `f${a.a}`, map1.mapOnClick);
-	/*
-	map.on('click', `f${a.a}`, a=> {
-		if (map1.src1 !== null && a.features.length > 0) {
-			map1.dt.features.forEach(b=>{if(b.properties.id == a.features[0].properties.id){
-				map.fitBounds(turf.bbox(b.geometry), {padding: 300})
-			}})
-		}
-	});
-	*/
-}
-
-const mapBox=()=>{
+const mapBox=a=>{
+//mapboxgl.accessToken = 'pk.eyJ1IjoicmV6YXBsZSIsImEiOiJjam1odmlld20zZmFjM3Bsazlybjk3cGJvIn0.mZTtCP_QNLKTrI-LUYYsrA';
+//mapboxgl.accessToken = 'pk.eyJ1IjoibWFsLXdvb2QiLCJhIjoiY2oyZ2t2em50MDAyMzJ3cnltMDFhb2NzdiJ9.X-D4Wvo5E5QxeP7K_I3O8w';
+mapboxgl.accessToken = 'pk.eyJ1IjoicXVlMzIxNiIsImEiOiJjaWhxZmMxMDUwMDBzdXhsdWh0ZDkyMzVqIn0.sz3lHuX9erctIPE2ya6eCw';
 	
 	map = new mapboxgl.Map({
 		container: 'map', // container ID
@@ -324,12 +462,6 @@ const mapBox=()=>{
 	['scrollZoom', 'dragRotate', 'dragPan', 'keyboard', 'doubleClickZoom', 'touchZoomRotate'].forEach(a=>map[a].disable());
 	//==================================================================
 	map.on('load', () => {
-		map.setPadding({
-			top: 150,
-			left: 80,
-			bottom: 300,
-			right: 10
-		});
 		const mLayers = map.getStyle().layers;
 		//console.log(mLayers);
 		map.setPaintProperty('land', 'background-color', '#ffffff');
@@ -340,31 +472,49 @@ const mapBox=()=>{
 		map.setPaintProperty('country-label', 'text-color', '#c0c0c0');
 		mLayers.forEach(a=>{
 			if (['land', 'water', 'waterway-label', 'water-line-label', 'water-point-label', 'country-label'].indexOf(a.id) < 0) map.removeLayer(a.id);
-		})
-		mapLoadData({src:'province.json', lvl:1, name:'ADM1_PCODE'});
+		});
+		
+		a.forEach(b=>{b()});
 	});
-	return map;
 }
 
-const mapUnloadData=a=>{
-	if (map.getLayer(`f${a}`)) {
-		map.off('mousemove', `f${a}`, map1.mapOnMouseenter);
-		//map.off('mouseleave', `f${a}`);
-		map.off('click', `f${a}`, map1.mapOnClick);
-		map.removeLayer(`f${a}`);
-	}
-	map.getLayer(`l${a}`) && map.removeLayer(`l${a}`);
-	map.getSource(a) && map.removeSource(a);
+const reqData =(method, url)=>{
+	return new Promise((a,b)=> {
+		const c = new XMLHttpRequest();
+		c.open(method, url);
+		c.onload =()=>{
+			if (c.status >= 200 && c.status < 300) a(c.response);
+			else b({ status: c.status, statusText: c.statusText });
+		};
+		c.onerror =()=>{ b({ status: c.status, statusText: c.statusText }); };
+		c.send();
+	});
 }
-
+/*
+reqData('GET', 'http://example.com').then(a=>{ console.log(a); })
+.catch(a=>{ console.error('Augh, there was an error!', a); });
+*/
 const mapLoadData=a=>{
+	a.old && mapUnloadData(a.old);
+	map1.src1 = a.src;
+	map1.lvl = a.lvl,
+	map1.Prop.Name = a.name;
+	reqData('GET', `../dt2/${map1.src1}`).then(b=>{
+		map1.dt = JSON.parse(b);
+		addSrc1({a:map1.src1, b:map1.dt});
+		if (map1.dt.properties.bounds) map.fitBounds(map1.dt.properties.bounds);
+		a.next && a.next(a.nextDt);
+	}).catch(b=>{ console.error('Augh, there was an error!', b); });
+}
+
+const mapLoadData1=a=>{
 	a.old && mapUnloadData(a.old);
 	map1.src1 = a.src;
 	map1.lvl = a.lvl,
 	map1.Prop.Name = a.name;
 	
 	const b = new XMLHttpRequest();
-	b.open('GET', `data/${map1.src1}`);
+	b.open('GET', `../dt2/${map1.src1}`);
 	b.onreadystatechange=()=>{
 		if (b.readyState == 4 && b.status == 200) {
 			map1.dt = JSON.parse(b.responseText);
@@ -374,11 +524,13 @@ const mapLoadData=a=>{
 	}
 	b.send();
 }
-
-const NavLeftSwitch=a=>{
-	const b = document.querySelector('#NavLeft > div > img.checked');
-	b && b.classList.remove('checked');
-	a.target.classList.add('checked');
+const mapLoadLabel=a=>{
+	a.old && mapRemoveLabel1(a.old);
+	map1.label1.src = a.src;
+	reqData('GET', `../dt2/${map1.label1.src}`).then(c=>{
+		map1.label1.dt = JSON.parse(c);
+		mapAddLabel1({a:map1.label1.src, b:map1.label1.dt});
+	}).catch(c=>{ console.error('Augh, there was an error!', c); });
 }
 
 const saveAs1=(a,b)=>{
@@ -410,7 +562,7 @@ const popup1 = el({a:'div', b:document.body, c:{style:'position:fixed;top:2px;le
 	
 	//el({a:'div', b:document.body, c:{id:'map'}});
 	el({a:'div', b:a, c:{id:'map'}});
-	mapBox();
+	mapBox([map1.init]);
 	hiChartJs(el({a:'div', b:a, c:{id:'ChartBottom'}}));
 	
 	elm.title = el({a:'span', b:el({a:'div', b:a, c:{id:'divTittle'}})});
